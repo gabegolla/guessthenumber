324c82b0546cee0ffcc400088be1fb732cb86293
gabegolla <gbariel.golla@gmail.com>
2020-04-19 15:19:20 +0200

    typos fixed

--- guessthenumber.py
+++ guessthenumber.py
@@ -2,7 +2,7 @@ import random
 number = random.randint(1, 100)
 attempts = 0
 
-user_number = int(input("I'm thinking about the number between 1 and 100... Can you guess it? Pleas type an integer:"))
+user_number = int(input("I'm thinking about the number between 1 and 100... Can you guess it? Please type an integer:"))
 
 while number != "guess":
     if user_number < number:
@@ -10,7 +10,7 @@ while number != "guess":
     elif user_number > number:
         print(f"Nope, {user_number} is toooo big!")
     else:
-        print(f"Yup, I was thinking about {user_number}! Congratulation, you needed {attempts} attempts to guess.")
+        print(f"Yup, I was thinking about {user_number}! Congratulations, you needed {attempts} attempts to guess.")
         break
     attempts += 1
     if attempts == 3 and number <= 10:
@@ -26,3 +26,4 @@ while number != "guess":
     elif attempts == 12:
         print(f"I thought it would be faster... This is a number between {(number - 2)} and {(number +2)}.")
     user_number = int(input("Try one more time: "))
+    
